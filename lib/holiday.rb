522313c2b8328e4f9afe82ecb03ad7ddcc8aef73
Marina Paskal <paskalm@rogers.com>
2017-11-02 01:38:58 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,46 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+  return holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  return holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
 
+   holiday_hash.map{|key,value|
+    if key == season
+       value[holiday_name] = supply_array
+    end
+  }
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  arr = []
+  holiday_hash.map{|key,value|
+    value.map{|k,v|
+      arr << v
+    }
+  }
+  return arr.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +68,26 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each{|key,value|
+    puts "#{key.to_s.capitalize!}:"
+        value.each{|k,v|
+          kval = k.to_s.split("_").map(&:capitalize).join(" ")
+           puts "  #{kval}: #{v.join(", ")}"
+        }
+  }
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  # include the string "BBQ
+  n = "BBQ"
+  arr = []
+  holiday_hash.each{|k,v|
+    v.each{|key,value|
+      if value.include? n
+        arr << key
+      end
+    }
+  }
+  return arr.flatten
 end
-
-
-
-
-
-
-
